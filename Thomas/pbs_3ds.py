bb1ce5e7d60eb5c9b3c2f7e21338a161e09a85ec
david <dcorigliano@student.ethz.ch>
2021-12-07 18:07:54 +0100

    Refinement of mesh generator

--- Thomas/pbs_3ds.py
+++ Thomas/pbs_3ds.py
@@ -241,7 +241,7 @@ class Simulation(object):
     # ----------------------------------------------------------------------------------------------
     # ----------------------------------------------------------------------------------------------
 
-    def __init__(self, res=10):
+    def __init__(self, res=5):
         # create objects in the scene
 
         self.ellips_field = EllipsoidField(self.radii_array,
